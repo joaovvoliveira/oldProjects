26991ebfa8fc25dcc38553af8cb939e189a523fc
João Victor <jvictor_oliveira@outlook.com.br>
2025-02-15 14:12:10 +0000

    add-all-notes

---
+++ notes-only/agile-notes.js
@@ -0,0 +1,127 @@
+`Vantagens de metodologias ageis:
+
+${Scrum}
+
+Divide o trabalho em Iteracoes, chamadas Sprints, de 2 a 4 semanas.
+Tem papeis definidos de cada pessoa envolvida.
+Possui reunioes especificas como Daily, Sprint Planning, Sprint Review e Retrospectiva.
+
+${Kanban}
+
+Oferece um fluxo continuo, sem iteracoes fixas.
+Nao impoe papeis fixos, ha maior flexibilidade.
+Foca na visualizacao do fluxo de trabalho por meio de um quadro Kanban, representando os estagios do processo.`
+
+
+`${Metodologia_Scrum}`
+
+`${AlteracoesScrumGuide}
+
+- Reorganizacao do time, para apenas 3 papeis: Scrum Master, P.O e Developers
+- Podendo ser ate 10 pessoas no total.
+- Os times foram declarados como "Autonomos", tendo a responsabilidade de gerenciar seu proprio trabalho.
+- Scrum Master tem um papel de lideranca, como um Lider que serve, e guia seu time.
+- P.O pode realizar o trabalho de delegar a responsabilidade(product backlog), pois esta mais proximo do cliente e entende melhor as prioridades.
+
+${ResponsabilidadesScrumMaster}
+
+- Lider, treinador e facilitador/Garante a Implementacao do Scrum
+- Remove os impedimentos do seu time, garantindo que todos estejam trabalhando com qualidade.
+- Facilitador das cerimonias do Scrum, garantindo que sejam eficazes. 
+
+${ResponsabilidadesP.O}
+
+- Responsavel por toda a comunicacao Cliente x Time, entendendo melhor as prioridades do Cliente. 
+- Cuida do Product Backlog, qualquer alteracao no mesmo.
+- Equilibrar demandas e qualquer realidade conflitante.
+- Tem que estar sempre disponivel para a equipe.
+- Tomada de decisoes.
+- Negociar as expectativas com o cliente e internamente.
+- Saber dar e aceitar feedbacks construtivos.
+
+${Desenvolvedores}
+
+- Necessarios abracas as mudancas, e aceitar com facilidade.
+- Manter seu foco na entrega de qualidade para o Cliente.
+- Trabalhar efetivamente com outros membros da equipe.
+- Responsabilidade coletiva, nao ha culpados, a responsabilidade e de todos.
+- Saber dar e aceitar feedbacks construtivos.
+- Garantir o envolvimento continuo do cliente ao longo do desenvolvimento.
+
+${Metodologia_Kanban}
+
+${Papeis_Kanban}
+
+${SRM} SERVICE REQUEST MANAGER
+
+- Compreender necessidades e expectativas do Cliente.
+- Facilita a priorizacao de itens de trabalho. 
+- Executa as mesmas funcoes do P.M/P.O/S.M
+
+${SDM} Service Delivery Manager
+
+- Responsavel pelo fluxo de trabalho, remove impedimentos do time.
+- Facilita as meetings e elabora o Delivery Planning.
+- Conhecido como: Gestor de Fluxo/Gerente de Entrega
+
+
+${ScrumBan}? Aplica as praticas e metodologias do SCRUM, e assim que estiver funcionando certinho, implementaria o KANBAN por cima do Scrum.
+
+${ScrumMaster}
+
+- Alem de tudo estipulado no SCRUM, ele e responsavel tambem pela melhoria continua do processo, incorporando principios do Kanban.
+- Concentra-se mais na eficiencia do fluxo de trabalho e nas possibilidades de otimizar o processo.
+
+${ProductOwner}
+
+- Priorizacao continua do backlog, com uma frequencia maior, nao apenar no inicio de cada sprint.
+- Deve estar aberto a ajustes dinamicos nas prioridades a medida que demandas novas/alteracoes surgem.
+
+${Desenvolvedores}
+
+- O trabalho pode fluir de forma continua e flexivel.
+- Foco em WIP - Eles sao responsaveis por gerenciar seu proprio trabalho, respeitando os limites de WIP(Work in Progress).
+- Determinar um limite para cada fase do trabalho, o que evita gargalos e mantem uma gestao eficaz do fluxo de trabalho.
+
+${Papeis_Comuns_na_Agilidade}
+
+${Stakeholders}
+
+- Pessoas interessadas no projeto. Fornecem feedback, prioridades e requisitos para o P.O.
+
+${TechLead}
+
+- Um lider da equipe de Desenvolvedores, uma referencia tecnica no time.
+- Ajuda a tomar decisoes relacionadas a arquitetura, design e implementacao.
+
+${EquipeOperacoes.OPS}
+
+- Garante que a infraestrutura e os sistemas estejam operacionais(DevOps).
+
+${Agilista}
+
+- Defensor da Pratica de Agilidade.
+- Alguem que acredita nos valores e principios ageis e divulga/valoriza isso entre os times.
+- Se envolve no projeto, participando de reunioes e fornecendo suporte durante o dia a dia e em todas as sprints.
+- Oferece consideracoes no final do projeto, feedbacks construtivos e criticos sobre o time. 
+
+${AgileCoach}
+
+- Fornece orientacao e coaching para as equipes e a organizacao, com fim de melhorar a agilidade.
+- Atua oferecendo treinamento, facilitacao e suporte para promover a autossuficiencia do time.
+
+
+${P.M/P.O}
+
+- P.O Se concentra no backlog e nas prioridades.
+- P.M Tem uma visao mais ampla, incluindo estrategias de mercado e alinhamento com objetivos de negocios.
+
+- P.O Trabalha junto ao time de desenvolvimento.
+- P.M Pode interagir, mas nao esta ligado diretamente.
+
+- P.O Detalha requisitos especificos para o storytelling durante as sprints.
+- P.M Define requisitos mais amplos e estrategicos, as x focando a longo prazo.
+
+- Prioridades definidas pelo P.O devem ser alinhadas com a visao estrategica do P.M.
+- Precisam ter uma comunicacao clara e objetiva entre ele.
+`
