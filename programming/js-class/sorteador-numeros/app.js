c71ebe8cc4d615a928c0287c735c6bdf39916fb6
João Victor <jvictor_oliveira@outlook.com.br>
2025-03-19 12:27:22 +0000

    endDrawnNumberJS

--- programming/js-class/sorteador-numeros/app.js
+++ programming/js-class/sorteador-numeros/app.js
@@ -1,16 +1,50 @@
-function draw(){
-  let qtd = document.getElementById('quantidade').value;
-  let de = document.getElementById('from').value;
-  let ate = document.getElementById('to').value;
+function draw() {
+  let qtd = parseInt(document.getElementById('qtd').value);
+  let from = parseInt(document.getElementById('from').value);
+  let to = parseInt(document.getElementById('to').value);
+  
+  let drawn = [];
+  let number;
 
-  alert(`Qtd: ${qtd}`);
-  alert(`from: ${from}`);
-  alert(`to: ${to}`);
 
-  let number = getRandomNumber(from,to);
-  alert(number)
+  for (let i = 0; i < qtd; i++) {
+    number = getRandomNumber(from, to);
+
+    while (drawn.includes(number)) {
+      number = getRandomNumber(from, to);
+    }
+
+    drawn.push(number);
+  }
+
+
+  let results = document.getElementById('results');
+  results.innerHTML = `<label class="texto__paragrafo">Números sorteados: ${drawn}</label>`
+  
+  allowRestart();
 }
 
-function getRandomNumber(min,max){
-  return Math.random() * (max-min) +min;
+function getRandomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+
+function allowRestart() {
+  let btnRestart = document.getElementById('btn-reiniciar');
+  if(btnRestart.classList.contains('container__botao-desabilitado')){
+    btnRestart.classList.remove('container__botao-desabilitado');
+    btnRestart.classList.add('container__botao');
+  } else {
+    btnRestart.classList.remove('container__botao');
+    btnRestart.classList.add('container__botao-desabilitado');
+  }
+}
+
+function restart(){
+  document.getElementById('qtd').value = '';
+  document.getElementById('from').value = '';
+  document.getElementById('to').value = '';
+  document.getElementById('results').innerHTML = `<label class="texto__paragrafo">Números sorteados: Não há números Sorteados. </label>`;
+
+  allowRestart();
+}
+
