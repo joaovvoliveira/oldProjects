4bc0199b3d25d4958c34ea30ea27b7a619084703
João Victor <jvictor_oliveira@outlook.com.br>
2025-03-25 00:06:25 +0000

    crudList

--- rocketseat-03-25/lists.py
+++ rocketseat-03-25/lists.py
@@ -16,13 +16,6 @@ showHomework()
 
 print('Getting it done...')
 
-# newlist = [n * 2 for n in list if n > 4]
-
-# listn = [n for n in test if n < 1400]
-# test = [ n for n in range(1,2456) if n % 3 ==0]
-# [ (n,n*3) for n in range(1,11)]
-# list3 = [n.lower() for n in lista2]
-
 
 def completeHomework(task):
     global listHomework
@@ -31,61 +24,74 @@ def completeHomework(task):
                     else t
                     for t in listHomework]
 
-    """Option for the line above"""
-    # newList = []
-    # for t in listHomework:
-    #     newList.append(t if t[0] != task else (task, 'Completed'))
-    # listHomework = newList
 
-    """Other Option ver the same code"""
+"""Option for the line above"""
+# newList = []
+# for t in listHomework:
+#     newList.append(t if t[0] != task else (task, 'Completed'))
+# listHomework = newList
+
+"""Other Option ver the same code"""
+#     if t[0] == task:
+#         newList.append((task, 'Completed'))
+#         print(newList)
+#     else:
+#         newList.append(t)
+
+
+def removeHomework(task):
+    global listHomework
+    listHomework = [t for t in listHomework if t[0] != task]
+
+    # for t in listHomework:
     #     if t[0] == task:
-    #         newList.append((task, 'Completed'))
-    #         print(newList)
-    #     else:
-    #         newList.append(t)
+    #         listHomework.remove(t)
+
+
+removeHomework('Mop Floor')
+print('--------')
+print(listHomework)
+
+# listHomework = [(t[0], 'Completed')
+#                     if t[0] == task
+#                     else t
+#                     for t in listHomework
 
 
 completeHomework('Mop Floor')
 showHomework()
-
-# homeworkPending = []
-# homeworkComplete = []
-
-
-# def newHomework(toDo):
-#     homeworkToDo = (toDo, "pending")
-#     homeworkPending.append(homeworkToDo)
+# (t[0], 'Completed')
+#                     if t[0] == task
+#                     else t
+#                     for t in listHomework]
+print('')
 
 
-# newHomework('Lavar a louca')
-# newHomework('Lavar a roupa')
-# newHomework('Lavar o chao')
+def getTasks(task):
+    listTask = [t for t in listHomework if t[0].lower() == task.lower()]
+    if listTask:
+        for title, status in listTask:
+            print(f'Found: {title}, status: {status}')
+    else:
+        print(f'Task not found: {task}')
 
 
-# def showHomework():
-#     for item in homeworkPending:
-#         print(f'{item[0]} - status: {item[1]}')
+"""1 way"""
+# for t in listHomework:
+#     if t[0] == tarefa:
+#         print(f'Tarefa encontrada: {t[0]} - Status: {t[1]}')
+# print(f'Nao achei {tarefa}')
+
+"""Other way"""
+# listPending = [(t[0], t[1]) for t in listHomework if t[1] == status]
+# print(listPending)
 
 
-# def completeHomework(task):
-#     global homeworkPending
-#     task = [(t[0], 'Completed') if t[0] for t in task in t[0] == task else t)]
-#     for item in homeworkPending:
-#         # homeworkComplete.append(
-#         #     item if item[0] == task else (task, 'Concluida'))
-
-#         # if item[0] == task:
-#         #     homeworkComplete.append((task, 'Concluida'))
-#         #     homeworkPending.remove(item)
-#         #     break
-#         # if item[0] == task:
-#         #     homework[1] = 'Completed'
-#     print(homeworkComplete)
-#     print(homeworkPending)
+getTasks('Wash the dishes')
 
 
-# completeHomework('Lavar a louca')
-
-# list = [1, 6, 4, 3, 7]
 # newlist = [n * 2 for n in list if n > 4]
-# print(newlist)
+# listn = [n for n in test if n < 1400]
+# test = [ n for n in range(1,2456) if n % 3 ==0]
+# [ (n,n*3) for n in range(1,11)]
+# list3 = [n.lower() for n in lista2]
